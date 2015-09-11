c86d99d9952217dcd099a4c9ea21fb965f2179aa
Oleg Mironov <allhakervbg@gmail.com>
2015-09-11 21:00:19 +0300

    Pagination

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 (function() {
-  var app = angular.module('exercise', ['exercise']);
+  var app = angular.module('exercise', ['exercise','ui.bootstrap']);
 
   app.factory('GitHub', function GitHub($http) {
     return {
@@ -24,6 +24,9 @@
       GitHub.searchRepos($scope.query, function (error, data) {
         if (!error) {
           $scope.repos = data.items;
+          $scope.totalItems = $scope.repos.length;
+          $scope.currentPage = 1;
+          $scope.itemsPerPage = 5;
         }
       });
     }
